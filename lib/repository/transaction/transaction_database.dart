7adee964b341f78e6d89c49d3985e25d36c2bec1
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2022-07-31 12:14:13 -0300

    Update transaction_database.dart

--- lib/repository/transaction/transaction_database.dart
+++ lib/repository/transaction/transaction_database.dart
@@ -1,9 +1,10 @@
+import 'package:finance_with_notion/shared/models/user_transaction_list_model.dart';
 import '../../shared/models/user_transaction.model.dart';
 
 abstract class TransactionDatabase {
   Future<void> insertTransaction(UserTransaction userTransaction);
 
-  Future<List<UserTransaction>> getTransactions();
+  Future<UserTransactionList> getTransactions();
 
   Future<void> deleteTransaction(String id);
 }
