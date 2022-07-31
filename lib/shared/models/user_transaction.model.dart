83fa2d8d768b446bdb22fa41f10aeb9645741972
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2022-07-31 12:14:02 -0300

    Update user_transaction.model.dart

--- lib/shared/models/user_transaction.model.dart
+++ lib/shared/models/user_transaction.model.dart
@@ -1,19 +1,35 @@
 import 'package:finance_with_notion/shared/models/enum/transaction_type.dart';
 
 class UserTransaction {
-  int id;
-  String description;
+  String id;
+  String note;
   double value;
   TransactionType transactionType;
-  DateTime date;
+  String date;
+  String category;
+  List<String> seconderyCategory;
+  String location;
+  String origin;
 
   UserTransaction(
-      this.id, this.description, this.value, this.transactionType, this.date);
+      this.id,
+      this.note,
+      this.value,
+      this.transactionType,
+      this.date,
+      this.category,
+      this.seconderyCategory,
+      this.location,
+      this.origin);
 
   UserTransaction.fromJson(Map<String, dynamic> json)
       : id = json["id"],
-        description = json["description"],
+        note = json["description"],
         value = json["value"],
         transactionType = json["transactionType"],
-        date = json["date"];
+        date = json["date"],
+        category = json["category"],
+        seconderyCategory = json["seconderyCategory"],
+        location = json["location"],
+        origin = json["origin"];
 }
