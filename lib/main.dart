6cd034edd84f55deee6c40d429ca92579b33d9d0
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2022-08-11 17:13:30 -0300

    set getIt in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:finance_with_notion/pages/initial/inital.page.dart';
 import 'package:finance_with_notion/shared/DI/di.dart';
+import 'package:get_it/get_it.dart';
+
+import 'shared/config/shared_prefs.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
-
+  GetIt getIt = GetIt.instance;
+  getIt.allReady().then((_) {
+    var preferences = getIt.get<SharedPrefs>();
+    preferences.notionDatabaseId = "database";
+    preferences.notionSecretToken = "secret";
+  });
   runApp(const InitialPage());
 }
