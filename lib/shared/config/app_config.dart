16d26005d161cf6c369aab34447674432d06f1b6
LeonardoLeiteMeira <42722914+LeonardoLeiteMeira@users.noreply.github.com>
2022-08-30 08:43:55 -0300

    added .env file

--- lib/shared/config/app_config.dart
+++ lib/shared/config/app_config.dart
@@ -1,17 +1,21 @@
 import 'package:finance_with_notion/shared/DI/di.dart';
 import 'package:finance_with_notion/shared/config/shared_prefs.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 
 class AppConfig {
   static Future<void> configApp() async {
+    await dotenv.load(fileName: ".env");
     configureDependencies();
     WidgetsFlutterBinding.ensureInitialized();
 
     GetIt getIt = GetIt.instance;
     await getIt.allReady();
     var preferences = getIt.get<SharedPrefs>();
-    preferences.notionDatabaseId = "database";
-    preferences.notionSecretToken = "secret";
+    preferences.notionDatabaseId =
+        dotenv.env["NOTION_DATABASE_ID"] ?? "databaseId";
+    preferences.notionSecretToken =
+        dotenv.env["NOTION_SECRET_TOKEN"] ?? "secretToken";
   }
 }
